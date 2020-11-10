e1691c9e11e203eaa899d9b698dd16da762a8fd6
William Smith <williamsmith@Services-MacBook-Pro.local>
2020-11-10 21:04:44 +1100

    equalise class weights, seems like model almost never predicts minority class correctly

--- MyERP.py
+++ MyERP.py
@@ -141,7 +141,8 @@ Y_test       = y[threeQuarters:]
 # the syntax is {class_1:weight_1, class_2:weight_2,...}. Here just setting
 # the weights all to be 1
 # class_weights = {0:1, 1:1, 2:1, 3:1}
-class_weights = dict(enumerate(class_weight.compute_class_weight('balanced', np.unique(Y_train), Y_train)))
+#class_weights = dict(enumerate(class_weight.compute_class_weight('balanced', np.unique(Y_train), Y_train)))
+class_weights = {0:1, 1:1}
 
 print('class_weights', class_weights)
 # convert labels to one-hot encodings.
