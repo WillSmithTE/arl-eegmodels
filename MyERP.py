3c5ff5cd688afa3b73c76119bb7684bb8d892c2e
William Smith <williamsmith@Services-MacBook-Pro.local>
2021-01-15 07:05:55 +1100

    batch 16, dropout 0.5

--- MyERP.py
+++ MyERP.py
@@ -148,7 +148,7 @@ print("chans:", chans, "samples:", samples)
 # configure the EEGNet-8,2,16 model with kernel length of 32 samples (other 
 # model configurations may do better, but this is a good starting point)
 model = EEGNet(nb_classes = 2, Chans = chans, Samples = samples, 
-               dropoutRate = 0.2, kernLength = 128, F1 = 8, D = 2, F2 = 16, 
+               dropoutRate = 0.5, kernLength = 128, F1 = 8, D = 2, F2 = 16, 
                dropoutType = 'Dropout')
 
 # compile the model and set the optimizers
@@ -174,7 +174,7 @@ checkpointer = ModelCheckpoint(filepath='/tmp/checkpoint.h5', verbose=1,
 # pretty noisy run-to-run, but most runs should be comparable to xDAWN + 
 # Riemannian geometry classification (below)
 ################################################################################
-fittedModel = model.fit(X_train, Y_train, batch_size = 1, epochs = 100, 
+fittedModel = model.fit(X_train, Y_train, batch_size = 16, epochs = 100, 
                         verbose = 2, validation_data=(X_validate, Y_validate),
                         callbacks=[checkpointer], class_weight = class_weights)
 
