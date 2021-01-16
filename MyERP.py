5d0b526a2686db57bb0b99a09ceb488acab1e2f9
William Smith <williamsmith@Services-MacBook-Pro.local>
2021-01-16 14:37:29 +1100

    20 epochs to try plot loss

--- MyERP.py
+++ MyERP.py
@@ -174,7 +174,7 @@ checkpointer = ModelCheckpoint(filepath='/tmp/checkpoint.h5', verbose=1,
 # pretty noisy run-to-run, but most runs should be comparable to xDAWN + 
 # Riemannian geometry classification (below)
 ################################################################################
-fittedModel = model.fit(X_train, Y_train, batch_size = 16, epochs = 50, 
+fittedModel = model.fit(X_train, Y_train, batch_size = 16, epochs = 20, 
                         verbose = 2, validation_data=(X_validate, Y_validate),
                         callbacks=[checkpointer], class_weight = class_weights)
 
