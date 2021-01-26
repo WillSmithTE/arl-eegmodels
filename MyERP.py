3332252cd51bd9d5e109eaa8dfe3730fb55787a9
William Smith <williamsmith@192-168-1-111.tpgi.com.au>
2021-01-26 18:58:57 +1100

    100 epochs

--- MyERP.py
+++ MyERP.py
@@ -179,7 +179,7 @@ checkpointer = ModelCheckpoint(filepath='/tmp/checkpoint.h5', verbose=1,
 # pretty noisy run-to-run, but most runs should be comparable to xDAWN + 
 # Riemannian geometry classification (below)
 ################################################################################
-fittedModel = model.fit(X_train, Y_train, batch_size = 16, epochs = 200, 
+fittedModel = model.fit(X_train, Y_train, batch_size = 16, epochs = 100, 
                         verbose = 2, validation_data=(X_validate, Y_validate),
                         callbacks=[checkpointer], class_weight = class_weights)
 
