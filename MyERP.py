bd78d57460179da8c404160abc8cc7fdff67dcb8
William Smith <williamsmith@192-168-1-111.tpgi.com.au>
2021-01-26 19:00:40 +1100

    spatialdropout2d

--- MyERP.py
+++ MyERP.py
@@ -154,7 +154,7 @@ F2 = F1 * D
 
 model = EEGNet(nb_classes = getNumClasses(), Chans = chans, Samples = samples, 
                dropoutRate = 0.25, kernLength = 64, F1 = F1, D = D, F2 = F2, 
-               dropoutType = 'Dropout')
+               dropoutType = 'SpatialDropout2D')
 
 # compile the model and set the optimizers
 model.compile(loss='categorical_crossentropy', optimizer='adam', 
