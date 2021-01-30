639bce6be29c756336eddd75a6d497db9cebcc16
William Smith <williamsmith@192-168-1-100.tpgi.com.au>
2021-01-30 21:32:16 +1100

    fix

--- MyERP.py
+++ MyERP.py
@@ -173,11 +173,14 @@ optimizer = Adam(lr=learningRate)
 
 # metrics = ['accuracy']
 
+def return0(a, b):
+    return 0.0
+
 def aucroc(y_true, y_pred):
     try:
-        return roc_auc_score(y_true, y_pred)
+        return py_func(roc_auc_score, (y_true, y_pred), double)
     except:
-        return 0
+        py_func(return0, (y_true, y_pred), double)
 
 metrics = [aucroc]
 
