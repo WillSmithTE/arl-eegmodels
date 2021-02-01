91b31b25fbb95b3c52436ae330dd29aca6b20c7d
William Smith <williamsmith@192-168-1-100.tpgi.com.au>
2021-02-02 10:34:58 +1100

    setup experiment

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from csvUtil import writeRow
 
 experiment = ERPExperiment()
 experiment.multiTrainAndPredict()
+experiment.multiTrainAndPredict(batchSize=500)
+experiment.multiTrainAndPredict(batchSize=2000)
+experiment.multiTrainAndPredict(batchSize=3000)
+experiment.multiTrainAndPredict(batchSize=4000)
