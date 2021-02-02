7582a6acf4287a01d0843afd2d7fbefff87ed9bd
William Smith <williamsmith@192-168-1-100.tpgi.com.au>
2021-02-02 18:46:27 +1100

    more experiments

--- main.py
+++ main.py
@@ -2,11 +2,4 @@ from MyERP import ERPExperiment
 from csvUtil import writeRow
 
 experiment = ERPExperiment()
-experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.6, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.6, F1=16, D=2)
-experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.6, F1=16, D=1, learningRate=0.005)
-experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.6, F1=16, D=2, learningRate=0.005)
-experiment.multiTrainAndPredict(batchSize=300, epochs=300, dropoutRate=0.6, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=300, epochs=300, dropoutRate=0.6, F1=16, D=2)
-experiment.multiTrainAndPredict(batchSize=300, epochs=300, dropoutRate=0.6, F1=16, D=1, learningRate=0.005)
-experiment.multiTrainAndPredict(batchSize=300, epochs=300, dropoutRate=0.6, F1=16, D=2, learningRate=0.005)
+experiment.multiTrainAndPredict(batchSize=300, epochs=300, dropoutRate=0.6, F1=16, D=2, learningRate=0.0005, numberExperiments=4)
