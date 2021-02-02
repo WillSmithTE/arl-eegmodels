0441c739c9b260349f8dd8760779c185d9f4b0b2
William Smith <williamsmith@192-168-1-100.tpgi.com.au>
2021-02-02 11:07:28 +1100

    clear results

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from MyERP import ERPExperiment
 from csvUtil import writeRow
 
 experiment = ERPExperiment()
-experiment.multiTrainAndPredict()
-experiment.multiTrainAndPredict(batchSize=500)
-experiment.multiTrainAndPredict(batchSize=2000)
-experiment.multiTrainAndPredict(batchSize=3000)
-experiment.multiTrainAndPredict(batchSize=4000)
+experiment.multiTrainAndPredict(F1=16)
+experiment.multiTrainAndPredict(batchSize=500, F1=16)
+experiment.multiTrainAndPredict(batchSize=2000, F1=16)
+experiment.multiTrainAndPredict(batchSize=3000, F1=16)
+experiment.multiTrainAndPredict(batchSize=4000, F1=16)
+experiment.multiTrainAndPredict(batchSize=10000)
+experiment.multiTrainAndPredict(batchSize=10000, F1=16)
+
+experiment.multiTrainAndPredict(F1=16, D=1)
+experiment.multiTrainAndPredict(batchSize=500, F1=16, D=1)
+experiment.multiTrainAndPredict(batchSize=2000, F1=16, D=1)
+experiment.multiTrainAndPredict(batchSize=3000, F1=16, D=1)
+experiment.multiTrainAndPredict(batchSize=4000, F1=16, D=1)
+experiment.multiTrainAndPredict(batchSize=10000, F1=16, D=1)
