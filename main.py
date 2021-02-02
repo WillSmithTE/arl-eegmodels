23f41b402d30334c9b3264c010bab5d78723705e
William Smith <williamsmith@192-168-1-100.tpgi.com.au>
2021-02-02 15:01:55 +1100

    down to 60Hz now

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ from MyERP import ERPExperiment
 from csvUtil import writeRow
 
 experiment = ERPExperiment()
-experiment.multiTrainAndPredict(batchSize=3000)
-experiment.multiTrainAndPredict(batchSize=3000, dropoutRate=.65)
-experiment.multiTrainAndPredict(batchSize=3000, dropoutRate=.8)
-experiment.multiTrainAndPredict(batchSize=3000, learningRate=.0005, numberExperiments=4)
-experiment.multiTrainAndPredict(batchSize=3000, F1=16)
-experiment.multiTrainAndPredict(batchSize=3000, F1=32)
-experiment.multiTrainAndPredict(batchSize=3000, F1=64)
-experiment.multiTrainAndPredict(batchSize=3000, F1=128)
-experiment.multiTrainAndPredict(batchSize=3000, D=1, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=4000, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=4000, D=4)
+experiment.multiTrainAndPredict(batchSize=4000, D=4, F1=32)
+experiment.multiTrainAndPredict(batchSize=4000, D=8)
+experiment.multiTrainAndPredict(batchSize=4000, D=8, F1=16)
