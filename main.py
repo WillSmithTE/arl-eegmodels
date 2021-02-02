76d922a5ccb41639313d1117a6091cad68f74db1
William Smith <williamsmith@192-168-1-100.tpgi.com.au>
2021-02-02 18:03:54 +1100

    more experiments

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ from MyERP import ERPExperiment
 from csvUtil import writeRow
 
 experiment = ERPExperiment()
-experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.65, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=500, epochs=100, dropoutRate=0.65, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.7, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=500, epochs=100, dropoutRate=0.7, F1=16, D=1)
 experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.6, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=500, epochs=100, dropoutRate=0.6, F1=16, D=1)
-
-experiment.multiTrainAndPredict(batchSize=5000, epochs=300, dropoutRate=0.65, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=5000, epochs=100, dropoutRate=0.65, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=5000, epochs=300, dropoutRate=0.7, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=5000, epochs=100, dropoutRate=0.7, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=5000, epochs=300, dropoutRate=0.6, F1=16, D=1)
-experiment.multiTrainAndPredict(batchSize=5000, epochs=100, dropoutRate=0.6, F1=16, D=1)
+experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.6, F1=16, D=2)
+experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.6, F1=16, D=1, learningRate=0.005)
+experiment.multiTrainAndPredict(batchSize=500, epochs=300, dropoutRate=0.6, F1=16, D=2, learningRate=0.005)
+experiment.multiTrainAndPredict(batchSize=300, epochs=300, dropoutRate=0.6, F1=16, D=1)
+experiment.multiTrainAndPredict(batchSize=300, epochs=300, dropoutRate=0.6, F1=16, D=2)
+experiment.multiTrainAndPredict(batchSize=300, epochs=300, dropoutRate=0.6, F1=16, D=1, learningRate=0.005)
+experiment.multiTrainAndPredict(batchSize=300, epochs=300, dropoutRate=0.6, F1=16, D=2, learningRate=0.005)
