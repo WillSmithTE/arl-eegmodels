9cd2a071434b267c20a556c17652a9c5a5c6075e
Will Smith <william.smith@service.nsw.gov.au>
2021-02-06 12:46:34 +1100

    more experiments

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from MyERP import ERPExperiment
 from csvUtil import writeRow
 
 experiment = ERPExperiment()
-experiment.multiTrainAndPredict(batchSize=64, epochs=500, dropoutRate=0.6, F1=16, D=4, learningRate=0.0005, numberExperiments=4)
-experiment.multiTrainAndPredict(batchSize=128, epochs=500, dropoutRate=0.6, F1=16, D=4, learningRate=0.0005, numberExperiments=4)
-experiment.multiTrainAndPredict(batchSize=256, epochs=500, dropoutRate=0.6, F1=16, D=4, learningRate=0.0005, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=64, dropoutRate=0.6, F1=16, D=4, learningRate=0.0005, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=128, dropoutRate=0.6, F1=16, D=4, learningRate=0.0005, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=256, dropoutRate=0.6, F1=16, D=4, learningRate=0.0005, numberExperiments=4)
