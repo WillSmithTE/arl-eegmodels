796f52a75d68d14e4104e06771eac938e416ea89
Will Smith <william.smith@service.nsw.gov.au>
2021-02-06 12:50:00 +1100

    more experiments

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from MyERP import ERPExperiment
 from csvUtil import writeRow
 
 experiment = ERPExperiment()
-experiment.multiTrainAndPredict(batchSize=64, dropoutRate=0.6, F1=16, D=4, learningRate=0.0005, numberExperiments=4)
-experiment.multiTrainAndPredict(batchSize=128, dropoutRate=0.6, F1=16, D=4, learningRate=0.0005, numberExperiments=4)
-experiment.multiTrainAndPredict(batchSize=256, dropoutRate=0.6, F1=16, D=4, learningRate=0.0005, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=64, dropoutRate=0.6, F1=16, D=1, learningRate=0.0005, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=128, dropoutRate=0.6, F1=16, D=1, learningRate=0.0005, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=256, dropoutRate=0.6, F1=16, D=1, learningRate=0.0005, numberExperiments=4)
+
+experiment.multiTrainAndPredict(batchSize=100, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=200, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=300, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4)
+experiment.multiTrainAndPredict(batchSize=400, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4)
