9056386f4dd9dc0580dfad82de638fabf8e0cd84
Will Smith <william.smith@service.nsw.gov.au>
2021-02-06 16:17:49 +1100

    fix experiment

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ experiment.multiTrainAndPredict(batchSize=128, dropoutRate=0.6, F1=16, D=1, lear
 experiment.multiTrainAndPredict(batchSize=256, dropoutRate=0.6, F1=16, D=1, learningRate=0.0005, numberExperiments=4)
 
 experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4, epochs=100)
-experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4, epochs=100)
-experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4, epochs=100)
-experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4, epochs=100)
+experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4, epochs=200)
+experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4, epochs=300)
+experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.6, F1=16, D=1, learningRate=0.001, numberExperiments=4, epochs=400)
 
 # ssvepExperiment = SSVEPExperiment()
 # ssvepExperiment.multiTrainAndPredict()
