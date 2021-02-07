b8dab8cc620e434258d7675a6c7f6df7dc1cdd29
Will Smith <william.smith@service.nsw.gov.au>
2021-02-08 06:57:01 +1100

    batch 2000

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from csvUtil import writeRow
 # experiment = ERPExperiment()
 # experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.5, F1=16, D=1, learningRate=0.001, numberExperiments=8)
 ssvepExperiment = SSVEPExperiment()
-ssvepExperiment.multiTrainAndPredict(batchSize=4000)
+ssvepExperiment.multiTrainAndPredict(batchSize=2000)
