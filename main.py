207bee22f5a62e9d6b1ed7a333f1faf78d064b11
Will Smith <william.smith@service.nsw.gov.au>
2021-02-08 06:44:58 +1100

    use ssvep

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MyERP import ERPExperiment
 from MySSVEP import SSVEPExperiment
 from csvUtil import writeRow
 
-experiment = ERPExperiment()
-experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.5, F1=16, D=1, learningRate=0.001, numberExperiments=8)
-# ssvepExperiment = SSVEPExperiment()
-# ssvepExperiment.multiTrainAndPredict()
+# experiment = ERPExperiment()
+# experiment.multiTrainAndPredict(batchSize=4000, dropoutRate=0.5, F1=16, D=1, learningRate=0.001, numberExperiments=8)
+ssvepExperiment = SSVEPExperiment()
+ssvepExperiment.multiTrainAndPredict()
