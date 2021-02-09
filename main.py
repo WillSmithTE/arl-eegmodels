3d3c0308f90e16ce65e27551038b28e4582a8619
Will Smith <william.smith@service.nsw.gov.au>
2021-02-09 19:44:26 +1100

    commit all

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from MySSVEP import SSVEPExperiment
 from csvUtil import writeRow
 
 experiment = ERPExperiment()
-experiment.multiTrainAndPredict(batchSize=30, dropoutRate=0.5, F1=4, D=2, learningRate=0.001)
-experiment.multiTrainAndPredict(batchSize=60, dropoutRate=0.5, F1=4, D=2, learningRate=0.001)
-experiment.multiTrainAndPredict(batchSize=120, dropoutRate=0.5, F1=4, D=2, learningRate=0.001)
+experiment.multiTrainAndPredict(batchSize=30, dropoutRate=0.5, F1=8, D=2, learningRate=0.001)
+experiment.multiTrainAndPredict(batchSize=16, dropoutRate=0.5, F1=8, D=2, learningRate=0.001)
+experiment.multiTrainAndPredict(batchSize=30, dropoutRate=0.5, F1=8, D=2, learningRate=0.001, epochs=500)
+experiment.multiTrainAndPredict(batchSize=30, dropoutRate=0.3, F1=8, D=2, learningRate=0.001)
 # ssvepExperiment = SSVEPExperiment()
 # ssvepExperiment.multiTrainAndPredict(batchSize=1000, epochs=500)
